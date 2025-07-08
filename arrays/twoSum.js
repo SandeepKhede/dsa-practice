3fc4f2167e0e7bc2fced92db50279bacfaedc67d
Sandeep Khede <sandeep@webkorps.com>
2025-07-08 21:13:33 +0530

    optimize approach hashmap

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -2,8 +2,10 @@
 // Output: [0, 1]  
 // Because nums[0] + nums[1] === 9
 
+
+//Brute Force
 const nums = [2, 7, 11, 15]
-const target = 200
+const target = 9
 function twoSum(nums, target) {
     for(let i=0; i<nums.length; i++){
         for(let j=i+1; j<nums.length; j++){
@@ -14,6 +16,24 @@ function twoSum(nums, target) {
         }
     }
     return false;
+}
+
+console.log(twoSum(nums,target));
+
+
+//using hashMap
+function twoSum (nums,target){
+    let obj = {}
+    
+    for(let i=0; i< nums.length; i++){
+        let diff = target - nums[i];
+        if(obj.hasOwnProperty(diff)){
+            return true;
+        }
+        obj[nums[i]]= i;
+    }
+
+    return false
 }
 
 console.log(twoSum(nums,target))
